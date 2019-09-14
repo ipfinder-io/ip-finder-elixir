929a2afcf3c08c33ed1032975f28afa2e16aa1cc
Mohamed Ben <mohamed@ipfinder.io>
2019-09-14 01:37:03 +0100

    update docs

--- lib/ipfinder/Validation/Domainvalidation.ex
+++ lib/ipfinder/Validation/Domainvalidation.ex
@@ -1,6 +1,21 @@
 defmodule Ipfinder.Validation.Domainvalidation do
   @moduledoc """
-  Documentation for Domainv alidation.
+
+  #  IPFinder elixir Client Library
+
+  The official elixir client library for the [IPFinder.io](https://ipfinder.io) get details for :
+  -  IP address details (city, region, country, postal code, latitude and more ..)
+  -  ASN details (Organization name, registry,domain,comany_type, and more .. )
+  -  Firewall by supported formats details (apache_allow,  nginx_deny, CIDR , and more ..)
+  -  IP Address Ranges by the Organization name  details (list_asn, list_prefixes , and more ..)
+  -  service status details (queriesPerDay, queriesLeft, key_type, key_info)
+  - Get Domain IP (asn, organization,country_code ....)
+  - Get Domain IP history (total_ip, list_ip,organization,asn ....)
+  - Get list Domain By ASN, Country,Ranges (select_by , total_domain  , list_domain ....)
+  - [GitHub ipfinder elixir](https://github.com/ipfinder-io/ip-finder-elixir)
+  - [ipfinder](https://ipfinder.io/)
+
+  ## Documentation for Domainv alidation.
   """
   @moduledoc since: "1.0.0"
 
